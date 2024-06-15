5cdeff344252992a5e2bbb17dc10b674e705566e
huzaifaghazali <huzaifaghazali@gmail.com>
2024-06-15 21:12:24 +0500

    Fetch dummy data

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-function Page() {
-  return <h1> Cabins Page</h1>;
+async function Page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Page;
