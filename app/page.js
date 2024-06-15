27b717ac33ef7edff3789dab451e4296711fe704
huzaifaghazali <huzaifaghazali@gmail.com>
2024-06-15 14:49:26 +0500

    Create a layout and add logo

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
-import Link from "next/link";
-import Navigation from "./components/Navigation";
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <div>
-    <Navigation/>
       <h1>The wild Oasis. Welcome to paradise next</h1>
       <Link href='/cabins'>Explore luxury cabins</Link>
     </div>
