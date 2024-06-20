defb422fe1c5ee41d2a6866aadbb3d22cc7cfe1f
huzaifaghazali <huzaifaghazali@gmail.com>
2024-06-20 22:17:38 +0500

    Add nested Reservations page and profile page

--- app/account/page.js
+++ app/account/page.js
@@ -3,7 +3,11 @@ export const metadata = {
 };
 
 function page() {
-  return <h1>Your Account</h1>;
+  return (
+    <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
+      Welcome Huzaifa
+    </h2>
+  );
 }
 
 export default page;
