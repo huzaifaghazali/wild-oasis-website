1f954a68b6a3f23c6d56b47c1ca42e9363e884c2
huzaifaghazali <huzaifaghazali@gmail.com>
2024-07-09 12:11:18 +0500

    Add functionality to delete a booking

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
 import { supabase } from './supabase';
+import { getBookings } from './data-service';
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -25,6 +26,32 @@ export async function updateGuest(formData) {
   if (error) throw new Error('Guest could not be updated');
 
   revalidatePath('/account/profile');
+}
+
+export async function deleteBooking(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in');
+
+  // Get the bookings for the logged-in user
+  const guestBookings = await getBookings(session.user.guestId);
+  // Extract the IDs of the bookings
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  // If the booking ID is not in the list of bookings for the user, throw an error
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error('You are not allowed to delete this booking');
+  }
+
+  // Delete the booking from the database
+  const { error } = await supabase
+    .from('bookings')
+    .delete()
+    .eq('id', bookingId);
+
+  if (error) throw new Error('Booking could not be deleted');
+
+  // Revalidate the cache for the user's reservations page
+  revalidatePath('/account/reservations');
 }
 
 export async function signInAction() {
